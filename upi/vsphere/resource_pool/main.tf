ece036623ea1bb3ec3a2a9203e96add727a6fd39
jkantihub <jkantihub@users.noreply.github.com>
2019-11-25 16:08:53 +0800

    Update main.tf

--- upi/vsphere/resource_pool/main.tf
+++ upi/vsphere/resource_pool/main.tf
@@ -3,7 +3,13 @@ data "vsphere_compute_cluster" "compute_cluster" {
   datacenter_id = "${var.datacenter_id}"
 }
 
-resource "vsphere_resource_pool" "resource_pool" {
-  name                    = "${var.name}"
-  parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
+#resource "vsphere_resource_pool" "resource_pool" {
+#  name                    = "${var.name}"
+#  parent_resource_pool_id = "${data.vsphere_compute_cluster.compute_cluster.resource_pool_id}"
+#}
+
+data "vsphere_resource_pool" "resource_pool" {
+  name                    = "${var.vsphere_cluster}/Resources"
+  datacenter_id = "${var.datacenter_id}"
 }
+
